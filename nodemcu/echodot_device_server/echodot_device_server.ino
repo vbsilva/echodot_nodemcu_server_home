72b689bd337aef5166b0eaccdaddae89807b2bb9
vbsilva <vbas@cin.ufpe.br>
2020-02-08 00:15:24 -0300

    adding relay and debug

--- nodemcu/echodot_device_server/echodot_device_server.ino
+++ nodemcu/echodot_device_server/echodot_device_server.ino
@@ -8,15 +8,15 @@
 #include "fauxmoESP.h"
 #include <ArduinoJson.h>
 
-
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 
 const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
 IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
 
-bool webRequest = false;
+const uint16_t fanRelayPin = 16; // D0
 
+bool webRequest = false;
 
 #include "credentials.h"
 #include "myir_raw.h"
@@ -24,6 +24,9 @@ bool webRequest = false;
 fauxmoESP fauxmo;
 AsyncWebServer server(80);
 StaticJsonDocument<200> doc;
+
+String debug;
+
 
 // -----------------------------------------------------------------------------
 
@@ -59,7 +62,7 @@ void ac_pwr() {
 }
 
 void tv_pwr() {
-  irsend.sendRaw(TV_PWR, TC_LEN, IR_FREQ);
+  irsend.sendRaw(TV_PWR, TV_LEN, IR_FREQ);
 }
 
 void serverSetup() {
@@ -67,7 +70,7 @@ void serverSetup() {
 
     // These two callbacks are required for gen1 and gen3 compatibility
     server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-        Serial.println("Running");
+        Serial.println("Request" + request->url());
         if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), String((char *)data))) return;
         // Handle any other body request here...
         if (request->url() == "/index.html" && request->method() == HTTP_POST) {
@@ -77,7 +80,7 @@ void serverSetup() {
             request->send(400, "text/plain", err.c_str());
           } else {
             webRequest = true;
-            request->send(200, "text/plain", "processing input");
+            request->send(200, "text/plain", debug);
           }
 
         }
@@ -97,6 +100,8 @@ void processWebRequest() {
   for (JsonPair p : obj) {
     int value = p.value().as<int>();
     const char* key = p.key().c_str();
+    Serial.println(key);
+    Serial.println(value);
 
     if (value == 0) continue;
    
@@ -110,11 +115,15 @@ void setup() {
 
     irsend.begin();
 
+    pinMode(fanRelayPin, OUTPUT);
+    digitalWrite(fanRelayPin, LOW);
+
     Serial.begin(SERIAL_BAUDRATE);
     Serial.println();
     Serial.println();
 
     wifiSetup();
+    debug += "Connected\n";
 
     serverSetup();
 
@@ -126,17 +135,28 @@ void setup() {
 
     fauxmo.addDevice("air conditioner");
     fauxmo.addDevice("television");
+    fauxmo.addDevice("fan");
     
     fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
         
         // Callback when a command from Alexa is received. 
         
         Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+        debug += "\ndevice id: " + String(device_id) + " device_name: "+ device_name + " state: "+ String(state); 
 
         if (strcmp(device_name, "air conditioner") == 0) {
+          debug += "\nair conditioner if";
           ac_pwr();
         } else if (strcmp(device_name, "television") == 0) {
+          debug += "\ntv if";
           tv_pwr();
+        } else if (strcmp(device_name, "fan") == 0) {
+          state ? digitalWrite(fanRelayPin, HIGH) : digitalWrite(fanRelayPin, LOW);
+          debug += "\n digitalWrite on pin" + String(fanRelayPin);
+          Serial.print("digitalWrite fan pin: ");
+          Serial.println(fanRelayPin);
+          Serial.print("state: ");
+          Serial.println(state);
         }
 
     });
